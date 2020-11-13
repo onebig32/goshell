e5eab3d44461122c59b55759fd5bd96b899ec3de
Aze <335424774@qq.com>
2020-11-13 13:32:35 +0800

    优化前端拉代码脚本

--- gitpull.sh
+++ gitpull.sh
@@ -16,8 +16,21 @@ nowBranch=`git symbolic-ref --short -q HEAD`
 
 # 1.判断拉取是否当前分支
 if [ "$nowBranch" != "$branch" ];then
-  echo "Git拉取分支[$branch]不是当前分支[$nowBranch]!"
-  exit
+  # echo "Git拉取分支[$branch]不是当前分支[$nowBranch]!"
+
+  # 拉取远程分支
+  git fetch origin $branch:$branch
+  if [ $? != 0 ];then
+     echo "Git拉取分支[$branch]失败"
+     exit
+  fi
+
+  # 切换远程分支
+  git checkout $branch
+  if [ $? != 0 ];then
+     echo "Git切换远程分支[$branch]失败"
+     exit
+  fi
 fi
 
 # 2.执行git命令
